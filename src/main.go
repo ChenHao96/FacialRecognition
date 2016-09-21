76eb13ab8a10d5e34c918c0d2e707e1de6d5b1a3
陈昊 <949727157@qq.com>
2016-09-21 14:34:29 +0800

    修改解析问题

--- src/main.go
+++ src/main.go
@@ -8,14 +8,13 @@ import (
 
 func main() {
 
-//	resourcesPath := os.Getenv("GOPATH") + "/resources"
-//	imgPath := resourcesPath + "/xijingpin.jpg"
-//
-//	value := detection.DetectFaceImg(detection.DetectRequestParam{IMG:imgPath})
+	//resourcesPath := os.Getenv("GOPATH") + "/resources"
+	//imgPath := resourcesPath + "/xijingpin.jpg"
+	//
+	//value := detection.DetectFaceImg(detection.DetectRequestParam{IMG:imgPath})
 
 	value := detection.LandmarkFaceImg(detection.LandmarkRequestParam{FACE_ID:"0814532b8ca9afd3341dce872750792e",TYPE:"25p"})
 
 	body, _ := json.Marshal(value)
-
 	fmt.Println(string(body))
 }
